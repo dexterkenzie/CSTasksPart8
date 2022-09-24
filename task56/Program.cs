bdeaf2b4985b645912b33f71285070ab6a912028
«dashkovsky» <>
2022-09-24 17:50:11 +0300

    task 56 done

--- task56/Program.cs
+++ task56/Program.cs
@@ -9,3 +9,66 @@
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки 
 // с наименьшей суммой элементов: 1 строка
+
+int m = InputNumbers("Количество строк: ");
+int n = InputNumbers("Количество столбцов: ");
+int range = InputNumbers("Диапазон: от 1 до ");
+
+int[,] array = new int[m, n];
+CreateArray(array);
+WriteArray(array);
+
+int min = 0;
+int sum = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+    int tempSumLine = SumLineElements(array, i);
+    if (sum > tempSumLine)
+    {
+        sum = tempSumLine;
+        min = i;
+    }
+}
+
+Console.WriteLine($"\n№{min + 1} - строкa с наименьшей суммой элементов. \nСумма элементов в этой строке равна [{sum}].");
+
+
+int SumLineElements(int[,] array, int i)
+{
+    int sum = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sum += array[i, j];
+    }
+    return sum;
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+        }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
