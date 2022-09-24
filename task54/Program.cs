780d52f6b369513786a8408fbdb0562654ae4e45
«dashkovsky» <>
2022-09-24 17:42:39 +0300

    task 54 done

--- task54/Program.cs
+++ task54/Program.cs
@@ -9,3 +9,65 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+int m = InputNumbers("Количество строк: ");
+int n = InputNumbers("Количество столбцов: ");
+int range = InputNumbers("Диапазон: от 1 до ");
+
+int[,] array = new int[m, n];
+Console.WriteLine($"\nСгенерированный массив: ");
+CreateArray(array);
+WriteArray(array);
+
+Console.WriteLine($"\nОтсортированный массив: ");
+OrderArrayLines(array);
+WriteArray(array);
+
+void OrderArrayLines(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+        }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
